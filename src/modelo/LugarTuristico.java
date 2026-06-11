5c3f6d06ca0644ed32e8acd04627c529a44f52c5
jcastroj1-cj <jcastroj1@unicartagena.edu.co>
2026-06-11 07:06:10 -0500

    se termina la primera parte de la clase lugar turistic

--- src/modelo/LugarTuristico.java
+++ src/modelo/LugarTuristico.java
@@ -17,4 +17,40 @@ public class LugarTuristico {
         this.ciudad = ciudad;
         this.descripcion = descripcion;
     }
+    // GETTERS
+    public String getCodigo()      { return codigo; }
+    public String getNombre()      { return nombre; }
+    public String getTipo()        { return tipo; }
+    public String getCiudad()      { return ciudad; }
+    public String getDescripcion() { return descripcion; }
+
+    // SETTERS
+    public void setCodigo(String codigo)           { this.codigo = codigo; }
+    public void setNombre(String nombre)           { this.nombre = nombre; }
+    public void setTipo(String tipo)               { this.tipo = tipo; }
+    public void setCiudad(String ciudad)           { this.ciudad = ciudad; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+
+    @Override
+    public String toString() {
+        return "Código: " + codigo +
+                " | Nombre: " + nombre +
+                " | Tipo: " + tipo +
+                " | Ciudad: " + ciudad +
+                " | Descripción: " + descripcion;
+    }
+    Cuando lo tengas, agrega los últimos dos métodos equals y hashCode. ¿Listo?
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LugarTuristico)) return false;
+        LugarTuristico that = (LugarTuristico) o;
+        return codigo.equalsIgnoreCase(that.codigo);
+    }
+
+    @Override
+    public int hashCode() {
+        return codigo.toUpperCase().hashCode();
+    }
+
 }
