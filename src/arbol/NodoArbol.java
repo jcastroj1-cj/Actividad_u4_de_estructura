cf76ec1db571abd1a71ce79a41c2f49aa955f261
jcastroj1-cj <jcastroj1@unicartagena.edu.co>
2026-06-11 07:09:21 -0500

    se crea el packete arbol y y la clase nordoarbol

---
+++ src/arbol/NodoArbol.java
@@ -0,0 +1,26 @@
+package arbol;
+
+public class NodoArbol {
+
+    private LugarTuristico lugar;
+    private List<NodoArbol> hijos;
+
+    public NodoArbol(LugarTuristico lugar) {
+        this.lugar = lugar;
+        this.hijos = new ArrayList<>();
+    }
+
+    // GETTERS
+    public LugarTuristico getLugar() { return lugar; }
+    public List<NodoArbol> getHijos() { return hijos; }
+
+    // Agregar un hijo a este nodo
+    public void agregarHijo(NodoArbol hijo) {
+        hijos.add(hijo);
+    }
+
+    // Verificar si es hoja (sin hijos)
+    public boolean esHoja() {
+        return hijos.isEmpty();
+    }
+}
